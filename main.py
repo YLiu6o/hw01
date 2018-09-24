42a1cf3a4d82173702846288341044fe3db08362
YLiu6o <37677668+YLiu6o@users.noreply.github.com>
2018-09-24 23:35:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 for i in range(1,10):
     print(' '*(i-1)*8,end='')
-    for j in range(1,11-i):
+    for j in range(i,10):
         formula='{0:1}x{1:1}={2:<4}'.format(i,j,i*j)
         print(formula,end='')
     print()
